621ad76b51e9fda3db9ddeb54a436af2e654f64b
deathslut666 <146013366+deathslut666@users.noreply.github.com>
2024-06-20 18:55:34 -0600

    Ejemplo repo

---
+++ src/excepiones/Rutinas.java
@@ -0,0 +1,11 @@
+
+package excepiones;
+
+import javax.swing.JOptionPane;
+
+public class Rutinas {
+    //Programa que recibe solo numeros pares, si ingresa impares mostrara el error 
+    public void ejemplo1(){
+       int n=Integer.parseInt(JOptionPane.showInputDialog("Digite un valor:  "));
+    }
+}
