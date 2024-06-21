6906c89f08434395b70b0c1317189bbc1f2a3ba8
deathslut666 <146013366+deathslut666@users.noreply.github.com>
2024-06-20 19:24:37 -0600

    Siguiendo codigo

--- src/excepiones/Rutinas.java
+++ src/excepiones/Rutinas.java
@@ -7,5 +7,14 @@ public class Rutinas {
     //Programa que recibe solo numeros pares, si ingresa impares mostrara el error 
     public void ejemplo1(){
        int n=Integer.parseInt(JOptionPane.showInputDialog("Digite un valor:  "));
+       try {
+           if(n%2==0){
+               JOptionPane.showMessageDialog(null, "El valor ingresando es par: \n"+n);
+            } else {
+               throw new Exception("Erro: el numero ingresado es impar: \n" + n);
+            }    
+        } catch (Exception X) {
+    
+        }
     }
 }
